881d3c287c05e74a55b477fda137369ac0affb55
dibya.borah <dibyajyotiborah469@gmail.com>
2025-01-07 16:05:24 +0530

    helper map fc and a story(Vertical added)

--- src/stories/Stack.stories.tsx
+++ src/stories/Stack.stories.tsx
@@ -1,19 +1,30 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { ComponentProps } from "react";
+import { ComponentProps } from 'react';
 
 import Stack from '../components/Stack';
 
-type StoryProps = ComponentProps<typeof Stack>;
+type StoryProps = ComponentProps<typeof Stack> & {
+    numberOfChildren: number;
+};
 
 const meta: Meta<StoryProps> = {
     component: Stack,
     argTypes: {
+        numberOfChildren: {
+            options: [1, 5, 10],
+            control: {
+                type: 'select',
+            },
+        },
         orientation: {
             options: ['horizontal', 'vertical'],
             control: {
                 type: 'select',
             },
         },
+    },
+    args: {
+        numberOfChildren: 5,
     },
 };
 
@@ -22,8 +33,31 @@ export default meta;
 type Story = StoryObj<StoryProps>;
 
 export const Horizontal: Story = {
-    args: {},
-    render: ({ ...args }) => {
-        return <Stack {...args} />
+    args: {
+        orientation: 'horizontal',
     },
+    render: ({ numberOfChildren, ...args }) => {
+        return <Stack {...args}>{createChildren(numberOfChildren)}</Stack>
+    },
+};
+export const Vertical: Story = {
+    args: {
+        orientation: 'vertical',
+    },
+    render: ({ numberOfChildren, ...args }) => {
+        return <Stack {...args}>{createChildren(numberOfChildren)}</Stack>
+    },
+};
+
+function createChildren(numberOfChildren: number) {
+    return Array(numberOfChildren)
+      .fill(null)
+      .map((_, index) => {
+        return (
+            <div
+              key={index}
+              style={{ width: 100, height: 100, backgroundColor: 'red' }}
+            />
+        );
+      });
 };
